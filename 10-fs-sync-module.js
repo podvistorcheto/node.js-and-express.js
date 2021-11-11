3a447f9038d68540bf9b0bfa353c4f06294d221f
podvistorcheto <denev.s.deyan@gmail.com>
2021-11-11 14:49:13 +0000

    Node.js part 2

--- 10-fs-sync-module.js
+++ 10-fs-sync-module.js
@@ -1,12 +1,17 @@
-const {readFileSync, writeFileSync} = require('fs');
+const {
+    readFileSync,
+    writeFileSync
+} = require('fs');
 console.log('start...')
 // it is the same as
 // const fs = require('fs');
 // fs.readFileSync
-const first = readFileSync('./content/first.txt', 'utf8',);
-const second = readFileSync('./content/second.txt','utf8');
+const first = readFileSync('./content/first.txt', 'utf8', );
+const second = readFileSync('./content/second.txt', 'utf8');
 // console.log(first, second)
-writeFileSync('./content/result-sync.txt',`New file created: Here is the result: ${first}, ${second}`, {flag: 'a'})
+writeFileSync('./content/result-sync.txt', `New file created: Here is the result: ${first}, ${second}`, {
+    flag: 'a'
+})
 
 console.log('done with this task');
 console.log('starting next task...');
--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-const http = require('http');
-const server = http.createServer(function(request, response){
-    // console.log(request)
-    if(request.url === '/') {
-        response.end('Welcome to our home page')
-    }
-    else if(request.url === '/about'){
-        response.end('Here is the about page')
-    }
-    // response.write('Welcome to our home page')
-    else{
-    response.end(`<h1>Ooops!</h1><p>We can seem to find the page you are looking for</p><a href="/">back to home page</a>`)
-    }
-})
-server.listen(5000)
+const _ = require('lodash');
+
+const items = [1, [2, [3, [4]]]];
+const newItems = _.flattenDeep(items)
+console.log(newItems);
